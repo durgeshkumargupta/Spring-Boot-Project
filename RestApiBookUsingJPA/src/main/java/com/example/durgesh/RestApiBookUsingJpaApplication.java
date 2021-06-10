04ad23a498d6d54dbaecef4122200bbed5822d7a
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-06-10 22:10:47 +0530

    Add files via upload

---
+++ RestApiBookUsingJPA/src/main/java/com/example/durgesh/RestApiBookUsingJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.durgesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiBookUsingJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiBookUsingJpaApplication.class, args);
+	}
+
+}
