fdfb8fda1b2037a11492dae7ad2dfb9f4c0515e3
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-06-10 13:21:31 +0530

    Add files via upload

---
+++ HttpStatusHanding/src/main/java/com/example/durgesh/entities/Book.java
@@ -0,0 +1,52 @@
+package com.example.durgesh.entities;
+
+import java.io.Serializable;
+
+
+public class Book implements  Serializable
+{
+	private static final long serialVersionUID = 2668617337790812162L;
+	private Integer id;
+	private String tiltle;
+	private String author;
+	
+	public Book() {
+	}
+	
+	public Book(Integer id, String tiltle, String author) {
+		super();
+		this.id = id;
+		this.tiltle = tiltle;
+		this.author = author;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTiltle() {
+		return tiltle;
+	}
+	public void setTiltle(String tiltle) {
+		this.tiltle = tiltle;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", tiltle=" + tiltle + ", author=" + author + "]";
+	}
+	
+	
+
+}
---
+++ HttpStatusHanding/src/main/java/com/example/durgesh/service/BookService.java
@@ -0,0 +1,75 @@
+package com.example.durgesh.service;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import com.example.durgesh.entities.Book;
+
+
+@Component
+public class BookService 
+{
+	private static List<Book> book=new ArrayList<>();
+	
+	static
+	{
+		book.add(new Book(111,"Java","ABC"));
+		book.add(new Book(222,"Python","XYZ"));
+		book.add(new Book(333,"C++","PQR"));
+		book.add(new Book(444,"C","WXY"));
+		book.add(new Book(555,"JavaScript","MNB"));
+	
+	}
+	
+	//get all book
+	public List<Book> getAllBook()
+	{
+		return book;
+	}
+	
+	//get one book using id
+	public Book getBookById(int id)
+	{
+		Book book_name=null;
+		try {
+			book_name=book.stream().filter(e->e.getId()==id).findFirst().get();
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+		
+		return book_name;
+	}
+	
+	//Add new Book
+	public Book addBook(Book b)
+	{
+		book.add(b);
+		return b;
+	}
+	
+	
+	//delete one book using id
+	public void deleteBook(int bid)
+	{
+		book=book.stream().filter(b->b.getId()!=bid).collect(Collectors.toList());
+	}
+	
+	//update book data using id
+	public void updateBook(Book book,int id)
+	{
+		this.book.stream().map(b->
+		{
+			if (b.getId()==id)
+			{	
+				b.setTiltle(book.getTiltle());
+				b.setAuthor(book.getAuthor());
+			}
+			return b;
+		}).collect(Collectors.toList());
+	}
+
+}
