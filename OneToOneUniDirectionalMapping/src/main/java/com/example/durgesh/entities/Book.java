77e7e2bd4ef775ffc57519d124caeb94467fa56f
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-06-11 18:29:51 +0530

    Add files via upload

---
+++ OneToOneUniDirectionalMapping/src/main/java/com/example/durgesh/entities/Book.java
@@ -0,0 +1,69 @@
+package com.example.durgesh.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Book")
+public class Book implements  Serializable
+{
+	private static final long serialVersionUID = 2668617337790812162L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="Book_id")
+	private Integer id;
+	
+	@Column(name="Book_title")
+	private String title;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Author author;
+	
+	public Book() {
+	}
+	
+	public Book(Integer id, String title, Author author) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTiltle() {
+		return title;
+	}
+	public void setTiltle(String tiltle) {
+		this.title = tiltle;
+	}
+	public Author getAuthor() {
+		return author;
+	}
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", tiltle=" + title + ", author=" + author + "]";
+	}
+	
+	
+
+}
