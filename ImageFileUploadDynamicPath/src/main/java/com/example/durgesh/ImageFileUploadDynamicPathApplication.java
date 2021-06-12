faeac54f019366dac10144cfd8d608625e482802
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-06-12 20:24:20 +0530

    Add files via upload

---
+++ ImageFileUploadDynamicPath/src/main/java/com/example/durgesh/ImageFileUploadDynamicPathApplication.java
@@ -0,0 +1,13 @@
+package com.example.durgesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageFileUploadDynamicPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageFileUploadDynamicPathApplication.class, args);
+	}
+
+}
