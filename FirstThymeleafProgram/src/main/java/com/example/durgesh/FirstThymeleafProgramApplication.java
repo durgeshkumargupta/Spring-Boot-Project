1486f02aef97e41a59232a084d6ce198ee239a52
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2021-06-14 15:14:24 +0530

    Add files via upload

---
+++ FirstThymeleafProgram/src/main/java/com/example/durgesh/FirstThymeleafProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.durgesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstThymeleafProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstThymeleafProgramApplication.class, args);
+	}
+
+}
