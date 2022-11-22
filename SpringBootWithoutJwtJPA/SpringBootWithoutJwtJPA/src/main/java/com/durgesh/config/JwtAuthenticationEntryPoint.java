6bbae6db8eae3b45c212e4366437fe059c18a702
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2022-11-22 10:36:59 +0530

    Add files via upload

---
+++ SpringBootWithoutJwtJPA/SpringBootWithoutJwtJPA/src/main/java/com/durgesh/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package com.durgesh.config;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+
+	private static final long serialVersionUID = -6694689115290378257L;
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException {
+
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+	}
+}
